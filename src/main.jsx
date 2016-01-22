11f52411a5f53bf19d58f0e80e479eeb9706a64b
Umut ÇELİK <mail@umutcelik.com.tr>
2016-01-22 15:21:22 +0200

    skeletonReact

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Lista = require('./components/List.jsx');
+
+ReactDOM.render(<Lista />, document.getElementById('ingredients'));
